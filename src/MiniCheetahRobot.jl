6f28eca833e24bfe72225ac79c4065121248d7f2
James Foster <james.paul.foster@outlook.com>
2020-08-21 00:42:44 +0100

    Added mechanism() function

--- src/MiniCheetahRobot.jl
+++ src/MiniCheetahRobot.jl
@@ -2,4 +2,25 @@ module MiniCheetahRobot
 
 using RigidBodyDynamics
 
+packagepath() = joinpath(@__DIR__, "..", "deps")
+urdfpath() = joinpath(packagepath(), "cheetah_description", "mini_cheetah.urdf")
+
+function mechanism(::Type{T} = Float64;
+                   floating = true,
+                   remove_fixed_tree_joints = true,
+                   add_flat_ground = false) where {T}
+    mechanism = RigidBodyDynamics.parse_urdf(urdfpath(); scalar_type = T,
+                floating = floating, remove_fixed_tree_joints = remove_fixed_tree_joints)
+
+    remove_fixed_tree_joints && remove_fixed_tree_joints!(mechanism)
+
+    return mechanism
 end
+
+function __init__()
+    if !isfile(urdfpath())
+        error("Could not find $(urdfpath()). Please run `import Pkg; Pkg.build(\"MiniCheetahRobot\")`.")
+    end
+end
+
+end # module
